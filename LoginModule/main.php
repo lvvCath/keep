30d28cc3c9632d1d41872bfcd8f509193058717b
Catherine Justine Zuniga <qcjdzuniga@tip.edu.ph>
2022-02-09 22:48:29 +0800

    logout session

--- LoginModule/main.php
+++ LoginModule/main.php
@@ -5,6 +5,7 @@ session_start();
 <h1>WELCOME!</h1>
 <?php
 if(isset($_SESSION["useruid"])){
-
+    echo "<h2>Hello " . $_SESSION["useruid"] . "</h2>";
 }
 ?>
+<a href="includes/logout.inc.php">Logout</a>
