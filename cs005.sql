18a8bfdfd7a9a663e3589029a68428aa776db402
Catherine Justine Zuniga <qcjdzuniga@tip.edu.ph>
2022-02-26 11:22:50 +0800

    Update cs005.sql

--- cs005.sql
+++ cs005.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 15, 2022 at 02:13 PM
+-- Generation Time: Feb 26, 2022 at 04:16 AM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 8.0.11
 
@@ -43,10 +43,11 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`usersId`, `usersFirstName`, `usersLastName`, `usersEmail`, `usersUid`, `usersPassword`, `usersPwdDate`) VALUES
 (33, 'Rosalyn', 'Quenca', 'rose@gmail.com', 'Rosal_08', '$2y$10$Yqgb3n8TfndiftCh34ZN5ewwE4M0sjiwJVw/0lmviI6FW5DqqmocG', '2021-02-03'),
-(34, 'Bob', 'Smith', 'bobsmith002@gmail.com', 'bobby001', '$2y$10$90UQIYjfior/wslcjeAe3OlWzBK.KCHEoCDGyU3WTx18rVB1umyBG', '2022-01-21'),
+(34, 'Bob', 'Smith', 'bobsmith002@gmail.com', 'bobby001', '$2y$10$vACC22RWSokYHC7d8pEHSetwPri.FcjtlSImhK8756lkaiTk72FUC', '2022-02-20'),
 (35, 'Anne', 'Watson', 'appleanne@gmail.com', 'anne008', '$2y$10$Lvp0PSvl7DnVk8uG7C8jYeu9AjN0399CjZEskWPYebfLIEiCIQoo2', '2022-02-14'),
 (36, 'new', 'sample', 'sample@gmail.com', 'newuser', '$2y$10$TdxMN.hOuqK0yqxvnqnZaON0utXILlXj89dgsR1xFWrKpb294UIWK', '2022-02-14'),
-(37, 'justine', 'zuniga', 'justinezuniga@gmail.com', 'justine_i0', '$2y$10$HASeb6srOv3YoRWttBU1DOBd1yXtiMPu.McOZ3s3EZGI6QqO94HC.', '2021-12-01');
+(37, 'justine', 'zuniga', 'justinezuniga@gmail.com', 'justine_i0', '$2y$10$lF4yFKScBR92idjoB.M/.uxnDHMe0ZdtqGhQzJsNnRnKStVbt95kC', '2022-02-16'),
+(38, 'Andrea', 'Austin', 'andrea.austin@gmail.com', 'andrea', '$2y$10$z3HtPpUTvc/ETsUPTZL7zuFonapz56VHQcm3GT8cNZ6fnE2D7oeJy', '2022-02-22');
 
 --
 -- Triggers `users`
@@ -87,7 +88,15 @@ INSERT INTO `user_history` (`pwdId`, `pwdUserId`, `pwdPassword`, `pwdUpdateDt`) 
 (28, 37, '$2y$10$Mg5iP8/Zkk7gehD09oMkA./Ez1kiODZDNrW7qal9m3/unZJYFZIYG', '2022-02-14'),
 (29, 37, '$2y$10$ui4MK6k5bNImT5HcmRUKVuv66yjYFIR3nLZfyp8ymGc5uMd.KRaOS', '2022-02-14'),
 (30, 37, '$2y$10$T36TTVGasOk5r8EN5Jgmnep7X9P6aaloIwruK359IZErVfyQzMQcG', '2022-02-14'),
-(31, 37, '$2y$10$HASeb6srOv3YoRWttBU1DOBd1yXtiMPu.McOZ3s3EZGI6QqO94HC.', '2022-02-14');
+(31, 37, '$2y$10$HASeb6srOv3YoRWttBU1DOBd1yXtiMPu.McOZ3s3EZGI6QqO94HC.', '2022-02-14'),
+(32, 37, '$2y$10$c2NiKmqaDtVY6pZfPYcR9OMeKIa6Y7lCoiBqqV/I3MqD4UXl3m8zG', '2022-02-16'),
+(33, 37, '$2y$10$lF4yFKScBR92idjoB.M/.uxnDHMe0ZdtqGhQzJsNnRnKStVbt95kC', '2022-02-16'),
+(34, 34, '$2y$10$vACC22RWSokYHC7d8pEHSetwPri.FcjtlSImhK8756lkaiTk72FUC', '2022-02-20'),
+(35, 38, '$2y$10$jzB4qAkdCL2NkMLwlqnqveqRjPiGXE93FaDONRNkSRC5hivVPZUfS', '2022-02-21'),
+(36, 38, '$2y$10$GMKUCelHeCs3huZdP7K6xeuUV/CJsAHCWw0uYiYw1YW3g7h9WpaHu', '2022-02-21'),
+(37, 38, '$2y$10$UhfJCxBywPsyOZ3ta5r5TuhAXX/vkMJW9ISEURVFTNDIUPAfvlCD6', '2022-02-21'),
+(38, 38, '$2y$10$atEU73zaL7jKqq/LqSwj0uHz5olf4.KQh3/OzGRNqGynXto9GRyhu', '2022-02-21'),
+(39, 38, '$2y$10$z3HtPpUTvc/ETsUPTZL7zuFonapz56VHQcm3GT8cNZ6fnE2D7oeJy', '2022-02-22');
 
 --
 -- Indexes for dumped tables
@@ -116,13 +125,13 @@ ALTER TABLE `user_history`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `usersId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
+  MODIFY `usersId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
 
 --
 -- AUTO_INCREMENT for table `user_history`
 --
 ALTER TABLE `user_history`
-  MODIFY `pwdId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
+  MODIFY `pwdId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
 
 --
 -- Constraints for dumped tables
